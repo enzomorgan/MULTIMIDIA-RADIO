95279b88d4a35f22b5d47a07468e007b3f154aee
Enzo Morgan <112134107+enzomorgan@users.noreply.github.com>
2024-09-12 22:13:26 -0300

    Update music.ts

--- src/dados/music.ts
+++ src/dados/music.ts
@@ -8,24 +8,24 @@ export type Music = {
 
 export const musics: Music[] = [
     {
-        name: "MENTIRA ESTAMPADA NA CARA",
-        author: "Nattanzinho Lima",
+        name: "MENTIRA ESTAMPADA-NA-CARA",
+        author: "Nattanzinho-Lima",
        // description: "musica 01",
         urlAudio: "./audios/Mentira-Estampada-Na-Cara.mp3",
         image: "./imagens/NATTANZINHOLIMA.jpg"
     },
     {
-        name: "Beleza da rosa",
+        name: "Beleza-da-rosa",
         author: "Zezo",
        // description: "musica 02",
         urlAudio: "./audios/30-Beleza da rosa-Zezo Potiguar-30 Anos.mp3",
         image: "./imagens/ZEZO.jpg"
     },
     {
-        name: "Chão de Giz",
-        author: "Zé Ramalho",
+        name: "Chão-de-Giz",
+        author: "Zé-Ramalho",
        // description: "musica 03",
-        urlAudio: "./musicas/01-Zé-Ramalho-Chão de Giz.mp3",
+        urlAudio: "./musicas/01-Zé-Ramalho-Chão-de-Giz.mp3",
         image: "./imagens/ZERAMALHO.jpg"
     },
 ];
